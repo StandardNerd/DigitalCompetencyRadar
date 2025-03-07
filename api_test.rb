6fa78463445a9ec1e35263bc3f27bb1041c9094c
Joon-Ki Choi <joon.ch@gmail.com>
2025-03-07 10:43:58 +0100

    feat(llm): update LLM API test ruby script to read job posting file and write LLM response into file

--- api_test.rb
+++ api_test.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
 require 'openai'
 
 # API configuration
 access_token = ENV['ACADEMICCLOUD_API_KEY']
 api_base = 'https://chat-ai.academiccloud.de/v1'
-model = 'meta-llama-3.1-8b-instruct'
+model = 'llama-3.1-nemotron-70b-instruct'
+
+job_file_path = 'job_001.txt'
+job_content = File.read(job_file_path)
 
 # Start OpenAI client
 client = OpenAI::Client.new(
@@ -18,10 +23,12 @@ chat_completion = client.chat(
     model: model,
     messages: [
       { role: 'system', content: 'You are a helpful assistant' },
-      { role: 'user', content: 'How tall is the Eiffel tower?' }
+      { role: 'user', content: "Extrahiere die geforderten Kompetenzen aus der Stellenanzeige: #{job_content}" }
     ]
   }
 )
 
 # Print full response as JSON
 puts chat_completion
+
+File.write('response_1.txt', chat_completion.to_json)
